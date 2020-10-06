51dc0108fd41f225048f95593dc05929cef1cb36
Christine <christineluo@berkeley.edu>
2020-10-05 23:25:50 -0700

    Added return

--- api/api.go
+++ api/api.go
@@ -98,6 +98,7 @@ func getJSON(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(response, credentials.Username + "\n")
 		fmt.Fprintf(response, credentials.Password)
 	}
+	return
 }
 
 func signup(response http.ResponseWriter, request *http.Request) {
